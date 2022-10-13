3d1a8318257e12ce85b4996a4bb55353e91b0b4f
Unal <unaltugrulbulut@gmail.com>
2022-10-13 11:20:34 +0300

    getSize Method

--- src/test/java/unal/video2/DriverManageMethods.java
+++ src/test/java/unal/video2/DriverManageMethods.java
@@ -1,4 +1,20 @@
 package unal.video2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverManageMethods {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        //gives to me pixel of page size
+        System.out.println(driver.manage().window().getSize()); //----> (1052, 698)
+        driver.manage().window().maximize();
+        System.out.println(driver.manage().window().getSize()); //----> (1361, 730)
+
+
+
+    }
 }
